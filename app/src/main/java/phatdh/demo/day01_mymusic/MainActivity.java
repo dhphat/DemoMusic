d110c3b9d67e36e908a6d89124c44244a8cb365d
Do Huu Phat <dhphat@outlook.com>
2020-05-24 17:05:51 +0700

    [PhatDH] init
    [PhatDH] init

---
+++ app/src/main/java/phatdh/demo/day01_mymusic/MainActivity.java
@@ -0,0 +1,14 @@
+package phatdh.demo.day01_mymusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
